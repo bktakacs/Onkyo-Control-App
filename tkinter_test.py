cbb309adfad6da3b89e64a0356b435c76c77ac54
Ben <175220481+bktakacs@users.noreply.github.com>
2025-05-11 13:13:20 -0400

    built power toggle button

--- tkinter_test.py
+++ tkinter_test.py
@@ -6,25 +6,51 @@ root = Tk()
 root.title("welcoem to app")
 root.geometry('1000x500')
 
-lbl = Label(root, text='Control your Onkyo from here!')
-lbl.grid()
+l1 = Label(root, text='Control your Onkyo from here!')
+l1.grid(column=1, row=0)
 
-# txt = Entry(root, width=10)
-# txt.grid(column=1, row=0)
-
+power_stat = query_onkyo('PWRQSTN', expected_prefix='!1PWR', verbose=False).split('!1PWR')[1][:2]
+l2 = Label(root, text='Receiver: On' if power_stat == '01' else 'Receiver: Standby')
+l2.grid(column=0, row=1)
 
 def clicked_pwr_toggle():
-    pwr_status = get_power_status(receiver_ip=receiver_ip)
-    
-    send_command(receiver_ip, receiver_port, "PWR01")
-    lbl.configure(text='Receiver off')
+    pwr_status = query_onkyo(
+        'PWRQSTN', expected_prefix='!1PWR', verbose=False
+    ).split('!1PWR')[1][:2]
+    send_command('PWR00' if pwr_status == '01' else 'PWR01')
+    l2.configure(text='Receiver: On' if pwr_status == '00' else 'Receiver: Standby')
 
-btn = Button(root, text='Click here to power on your receiver',
+b1 = Button(root, text='Toggle Power',
              fg='red', command=clicked_pwr_toggle)
 
-btn.grid(column=2, row=0)
+b1.grid(column=1, row=1)
 
+# v2 = DoubleVar()
 
+# def show2():
+#     sel = 'Vertical Scale Value = ' + str(v2.get())
+#     l3.config(text=sel, font=('Courier', 14))
 
+# s2 = Scale(
+#     root,
+#     variable=v2,
+#     from_=100,
+#     to=0,
+#     orient=VERTICAL,
+# )
+
+# l4 = Label(root, text='Vertical Scaler')
+# b2 = Button(root, text='Display Vertical',
+#             command=show2,
+#             bg='purple',
+#             fg='white')
+
+# l3 = Label(root)
+# l3.grid()
+
+# s2.pack(anchor=CENTER)
+# l4.pack()
+# b2.pack()
+# l2.pack()
 
 root.mainloop()
