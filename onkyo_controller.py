77724b6fbac2a6e2c84e6419750070da71806943
Ben <175220481+bktakacs@users.noreply.github.com>
2025-05-26 14:53:51 -0400

    na

--- onkyo_controller.py
+++ onkyo_controller.py
@@ -86,9 +86,9 @@ def db_to_hex(vol_db):
 # Example: Power ON
 receiver_ip = "192.168.50.164"
 receiver_port = 60128
-volume = 40
+# volume = 40
 # command = "MVL" + str(db_to_hex(volume / 2))  # Add other codes like "PWR00", "MVL0A", etc.
-# command = "AMT00"
+# command = "MVL" + str(25)
 # send_command(command)
 
 # query_onkyo('AMTQSTN', verbose=True, expected_prefix='!1AMT')
