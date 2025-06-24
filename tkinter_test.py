d36416983dc2be4d1d062ecdb987d5166ad99b47
Ben <175220481+bktakacs@users.noreply.github.com>
2025-06-24 09:59:32 -0400

    added safeguard to listening mode query

--- tkinter_test.py
+++ tkinter_test.py
@@ -5,13 +5,16 @@ from onkyo_controller import *
 # DEFINE FUNCTIONS
 def periodic_query():
     global current_power_status
-    current_power_status = query_onkyo('PWRQSTN', expected_prefix='!1PWR', verbose=False).split('!1PWR')[1][:2]
+    current_power_status = query_onkyo('PWRQSTN', expected_prefix='!1PWR', verbose=True).split('!1PWR')[1][:2]
     l2.config(text='Receiver: On' if current_power_status == '01' else 'Receiver: Standby')
     wait_time = 1
     second = 1000
     minute = 1000 * 60
     root.after(wait_time * minute, periodic_query)
 
+################################################################################
+# General verbosity for debugging
+verbosity = False
 
 ################################################################################
 # MAIN LOOP
@@ -30,13 +33,13 @@ l1.grid(column=1, row=0)
 ################################################################################
 # LABEL 2 & BUTTON 1: POWER TOGGLE
 
-current_power_status = query_onkyo('PWRQSTN', expected_prefix='!1PWR', verbose=False).split('!1PWR')[1][:2]
+current_power_status = query_onkyo('PWRQSTN', expected_prefix='!1PWR', verbose=verbosity).split('!1PWR')[1][:2]
 l2 = Label(root, text='Receiver: On' if current_power_status == '01' else 'Receiver: Standby')
 l2.grid(column=0, row=1)
 
 def clicked_pwr_toggle():
     pwr_status = query_onkyo(
-        'PWRQSTN', expected_prefix='!1PWR', verbose=False
+        'PWRQSTN', expected_prefix='!1PWR', verbose=verbosity
     ).split('!1PWR')[1][:2]
     send_command('PWR00' if pwr_status == '01' else 'PWR01')
     l2.configure(text='Receiver: On' if pwr_status == '00' else 'Receiver: Standby')
@@ -54,7 +57,7 @@ l3 = Label(root,
 l3.grid(column=0, row=2)
 
 # query volume and set as current
-get_current_volume = query_onkyo('MVLQSTN', expected_prefix='!1MVL', verbose=False).split('!1MVL')[1][:2]
+get_current_volume = query_onkyo('MVLQSTN', expected_prefix='!1MVL', verbose=verbosity).split('!1MVL')[1][:2]
 
 current_volume = DoubleVar()
 current_volume.set(int(get_current_volume, 16)) # convert hex to decimal
@@ -78,7 +81,7 @@ slider.grid(column=1, row=2)
 # LABEL 4 & BUTTON 2: TOGGLE MUTE
 
 # get mute status
-get_mute_status = query_onkyo('AMTQSTN', expected_prefix='!1AMT', verbose=False).split('!1AMT')[1][:2]
+get_mute_status = query_onkyo('AMTQSTN', expected_prefix='!1AMT', verbose=verbosity).split('!1AMT')[1][:2]
 # display mute status
 l4 = Label(root,
            text='Mute: On' if get_mute_status == '01' else 'Mute: Off')
@@ -87,7 +90,7 @@ l4.grid(column=0, row=3)
 # mute toggle event
 def clicked_mute_toggle():
     temp_mute_status = query_onkyo(
-        'AMTQSTN', expected_prefix='!1AMT', verbose=False
+        'AMTQSTN', expected_prefix='!1AMT', verbose=verbosity
     ).split('!1AMT')[1][:2]
     send_command('AMT00' if temp_mute_status == '01' else 'AMT01')
     l4.configure(text='Mute: On' if temp_mute_status == '00' else 'Mute: Off')
@@ -174,8 +177,19 @@ lm_options = [
     'STEREO','DIRECT','FILM','ACTION','ALL CH STEREO'
 ]
 listening_mode = StringVar()
-listening_mode.set(lm_dict[query_onkyo('LMDQSTN', expected_prefix='!1LMD', verbose=False).split('!1LMD')[1][:2]]) # set to current listening mode
-# listening_mode.set(lm_options[0])
+
+# Check for good listening mode query response, else set to STEREO
+listening_mode_temp = query_onkyo('LMDQSTN', expected_prefix='!1LMD', verbose=verbosity)
+if listening_mode_temp and '!1LMD' in listening_mode_temp:
+    lm_code = listening_mode_temp.split('!1LMD')[1][:2]
+    if lm_code in lm_dict:
+        listening_mode.set(lm_dict[query_onkyo('LMDQSTN', expected_prefix='!1LMD', verbose=verbosity).split('!1LMD')[1][:2]]) # set to current listening mode
+    else:
+        print('Unknown or unsupported listening mode code: {code!r}. Setting to default.')
+        listening_mode.set('00')
+else:
+    print('No valid response from LMDQSTN query. Setting to default.')
+    listening_mode.set('00')
 
 # change listening mode
 def change_listening_mode(mode):
