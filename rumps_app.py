2ecfd1430b78dba3c87169527b63aa3309a0c5fa
ben <dtn125@alumni.ku.dk>
2025-06-26 18:01:44 -0400

    added sweet message

--- rumps_app.py
+++ rumps_app.py
@@ -333,6 +333,49 @@ class OnkyoStatusBarApp(rumps.App):
         # self.listener.daemon = True
         self.listener.start()
 
+"""
+It is my belief that I will get better results from the keyboard listener if I put the listener outside of the rumps class. It would look something like this:
+
+listener = keyboard.Listener(
+    on_press = on_key_press,
+    on_release = on_key_release,
+)
+listener.start()
+
+From the documentation for global hotkeys:pynput provides the class pynput.keyboard.HotKey for this purpose. It contains two methods to update the state, designed to be easily interoperable with a keyboard listener: pynput.keyboard.HotKey.press and pynput.keyboard.HotKey.release which can be directly passed as listener callbacks.
+
+def on_activate():
+    print('Global hotkey activated')
+
+def for_canonical(f):
+    return lambda k: f(l.canonical(k))
+
+hotkey = keyboard.HitKey(
+    keyboard.HotKey.parse('<ctrl>+<alt>+h'),
+    on_activate)
+)
+
+with keyboard.Listener(
+    on_press=for_canonical(hotkey.press),
+    on_release=for_canonical(hotkey.release)
+) as l:
+    l.join()
+
+This will create a hotkey, and then use a listener to update its state. Once all the specified keys are pressed simultaneously, on_activate will be invoked.
+
+Note that keys are passed through pynput.keyboard.Listener.canonical before being passed to the HotKey instance. This is to remove any modifier state from the key events, and to normalise modifiers with more than one physical button.
+
+The method pynput.keyboard.HotKey.parse is a convenience function to transform shortcut strings to key collections. Please see its documentation for more information.
+
+To register a number of global hotkeys, use the convenience class pynput.keyboard.GlobalHotKeys:
+
+
+with keyboard.GlobalHotKeys({
+        '<ctrl>+<alt>+h': on_activate_h,
+        '<ctrl>+<alt>+i': on_activate_i}) as h:
+    h.join()
+
+"""
 
 # --- Main Loop --- #
 OnkyoStatusBarApp().run()
