59a35b85d1d4b458981f0aad3a040c021380d9cb
Bence Takacs <dtn125@alumni.ku.dk>
2025-06-25 21:59:42 -0400

    removing rumps_addition file

--- rumps_addition.py
+++
@@ -1,7 +0,0 @@
-# add toggle mute to alt-r pageup key
-if keyboard.Key.alt_r in self.pressed_keys and keyboard.Key.pageup:
-    toggle_mute()
-
-# notification for mute. add to toggle_mute() function
-rumps.notification(title='Mute {}'.format(self.mute_status), subtitle=None, message=None, data=None, sound=False)
-
