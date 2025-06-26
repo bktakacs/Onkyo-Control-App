30628a70286112dd9e3e2a3f2d8c3724f47670b3
Ben <175220481+bktakacs@users.noreply.github.com>
2025-06-26 13:06:08 -0400

    updated for better build

--- setup_rumps.py
+++ setup_rumps.py
@@ -1,11 +1,14 @@
 # SETUP FOR RUMPS STATUS BAR ICON ONKYO CONTROLS
+import os
 
 from setuptools import setup
+from setuptools import find_packages
 
 APP = ['rumps_test.py']
 DATA_FILES = []
 OPTIONS = {
-    'argv_emulation': True,
+    'argv_emulation': False,
+    'iconfile': 'on.jpg',
     'plist': {
         'LSUIElement': True,
     },
@@ -13,9 +16,12 @@ OPTIONS = {
 }
 
 setup(
+    name='OnkyoControlApp',
     app=APP,
+    author='Ben Takacs',
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
+    packages=find_packages(),
     setup_requires=['py2app'],
 )
 
